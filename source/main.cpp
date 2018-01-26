c66be7b8bb3fb36295222e5a1ab6aac44182801b
Szu-Chi,Huang <r310102930719@gmail.com>
2018-01-26 23:22:35 +0800

    Finish Literal constant

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@ int main (){
 	/*Hello World!*/
 	cout << "Hello! World!\n";
 	cout << "«¢Åo! C++\n";
+	
 	/*Data Type*/
 	cout << "\n«¬ºA\t\t¤j¤p(bytes)";
 	cout << "\nint\t\t" << sizeof(int);
@@ -14,8 +15,11 @@ int main (){
 	cout << "\ndouble\t\t" << sizeof(double);
 	cout << "\nchar\t\t" << sizeof(char);
 	cout << "\n";
+	
 	/*Literal constant*/
-
+	cout << "sizeof(1): \t" << sizeof(1) << "\n";
+	cout << "sizeof(1.0)\t" << sizeof(1.0) << "\n";
+	
 	/*Variable*/
 
 	/*Input Output*/
